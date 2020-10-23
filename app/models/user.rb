ee2d2fd7c1425704834fbc212368ff192ab93d36
vanheaven <vanheaven6@gmail.com>
2020-10-23 16:33:55 +0300

    Add username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true,
-                   length: { minimum: 3 },
-                   uniqueness: true
+  validates :name, :username, presence: true,
+                   length: { minimum: 3 }              
+  validates :username, uniqueness: true
 
   has_many :authored_articles, foreign_key: :author_id, class_name: :Article
   has_many :votes
