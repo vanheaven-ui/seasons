c2872b8656994a55af362c20345af929ab37c064
vanheaven <vanheaven6@gmail.com>
2020-10-23 13:20:11 +0300

    Add custom login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  put 'edit_profile', to: 'sessions#update'
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
