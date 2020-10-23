291f6d3afa396ed1b3d736b4285dd21e426a7d83
vanheaven <vanheaven6@gmail.com>
2020-10-23 14:25:35 +0300

    AddScaffold with Article model

---
+++ spec/views/articles/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "articles/new", type: :view do
+  before(:each) do
+    assign(:article, Article.new(
+      title: "MyText",
+      text: "MyText",
+      author: nil
+    ))
+  end
+
+  it "renders new article form" do
+    render
+
+    assert_select "form[action=?][method=?]", articles_path, "post" do
+
+      assert_select "textarea[name=?]", "article[title]"
+
+      assert_select "textarea[name=?]", "article[text]"
+
+      assert_select "input[name=?]", "article[author_id]"
+    end
+  end
+end
