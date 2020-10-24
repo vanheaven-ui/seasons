afccc32ebd48c38b721781482b1b2525724f1d6f
vanheaven <vanheaven6@gmail.com>
2020-10-24 04:20:43 +0300

    Add basic header navigation bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, :username, presence: true,
-                   length: { minimum: 3 }              
+                              length: { minimum: 3 }              
   validates :username, uniqueness: true
 
   has_many :authored_articles, foreign_key: :author_id, class_name: :Article
