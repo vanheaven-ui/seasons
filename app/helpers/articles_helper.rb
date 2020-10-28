ce8384ceb9d421e14ebd681abb28679b2804e4ee
vanheaven <vanheaven6@gmail.com>
2020-10-28 20:19:35 +0300

    Modify vote/unvote functionality

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,10 +1,14 @@
 module ArticlesHelper
   
   def most_votes(articles)
+    most_voted_article = ''
     most_votes = 0
     articles.each do |article|
-      most_votes = article.votes.count if article.votes.count > most_votes
+      if article.votes.count > most_votes
+        most_votes = article.votes.count 
+        most_voted_article = article
+      end
     end
-    most_votes
+    most_voted_article
   end
 end
