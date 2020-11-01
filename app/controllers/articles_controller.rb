833201468748c9d28bcb3725707579e5bdf0c066
vanheaven <vanheaven6@gmail.com>
2020-11-02 00:17:17 +0300

    Updates for heroku deployment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @sorted_categories = Category.all.includes(:articles).order('priority').limit(4)
-    @most_voted_article = Article.find(Vote.group(:article_id).count.max_by { |_k, v| v }.first)
+    @most_voted_article = Article.find(Vote.where.not(article_id: nil).group(:article_id).count.max_by { |_k, v| v }.first)
     @articles = Article.all
     @users = User.all.preload(:authored_articles).limit(6)
   end
