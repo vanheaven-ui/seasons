076dfae4ce9f88d8473a5b65b4a644cc0a3a2638
vanheaven <vanheaven6@gmail.com>
2020-11-06 14:47:48 +0300

    Update app config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,5 @@ module Seasons
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end 
+  end
 end
