de15e5786d97b2eda37ab68a636213c963f6f1a4
vanheaven <vanheaven6@gmail.com>
2020-11-09 00:10:06 +0300

    Modify article db table as per TSE recommendation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_06_112454) do
+ActiveRecord::Schema.define(version: 2020_11_08_210156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_11_06_112454) do
     t.bigint "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
