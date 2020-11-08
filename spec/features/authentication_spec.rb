6835d28cf3f448b9c52097b89d0417bceabb9331
vanheaven <vanheaven6@gmail.com>
2020-11-09 00:46:48 +0300

    Add features rspec tests

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature 'Authentication', type: :feature do # rubocop: disable Metrics/BlockLength
+  before(:each) do
+    user = User.new(name: 'Ezekiel')
+    user.save
+  end
+
+  describe 'Login' do
+    scenario 'User can login successfully' do
+      visit '/login'
+      fill_in :username, with: :Ezekiel
+      click_button 'Login'
+      expect(page).to have_text('Your Access')
+    end
+
+    scenario 'User cannot sign in' do
+      visit '/login'
+      fill_in :name, with: :John
+      click_button 'Login'
+      expect(page).to have_text('Ops, something is wrong, please check entry')
+    end
+  end
+
+  describe 'Sign Up' do
+    scenario 'User can sign up' do
+      visit '/sign_up'
+      fill_in 'user[name]', with: :Robobo
+      click_button 'Create User'
+      expect(page).to have_text('Welcome to Seasons')
+    end
+  end
+end
