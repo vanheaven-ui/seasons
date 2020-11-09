313a0b4414532d85b73266e1e09bb2e681cb8e64
vanheaven <vanheaven6@gmail.com>
2020-11-09 23:01:47 +0300

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{ name: 'Love', priority: 1 },
+                              { name: 'Faith', priority: 2 },
+                              { name: 'Hope', priority: 3 },
+                              { name: 'Doubt', priority: 4 }
+                            ])
